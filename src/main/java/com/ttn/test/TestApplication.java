d6498b34ee51be5688d385e02c9ea62e7b61d088
DESKTOP-K7826F0\trang <ttndo@vietsugar.com.vn>
2026-01-10 10:11:00 +0700

    fix return

--- src/main/java/com/ttn/test/TestApplication.java
+++ src/main/java/com/ttn/test/TestApplication.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class TestApplication {
-    @GetMapping("/how")
+    @GetMapping("/")
     public String hi(){
-        return "how are you? bro?";
+        return "welcome home, how are you? bro?";
     }
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
